6e2de3214db2b568dedddebdbc3a5aa76f95aaac
Yaseen Hasan <yabuzeina02@gmail.com>
2025-09-27 02:34:59 +0300

    fix

--- HealthFlow/src/main/java/module-info.java
+++ HealthFlow/src/main/java/module-info.java
@@ -10,6 +10,11 @@ module com.example.healthflow {
     requires java.net.http;
     requires java.desktop;
 
+    // Ikonli
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
+
+
     // Open controllers package for JavaFX FXML
     opens com.example.healthflow.controllers to javafx.fxml;
 
